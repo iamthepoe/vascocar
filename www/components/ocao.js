4be713a5e8f90429bdcd2f0f3a09735742afbab5
iamthepoe <lucagamerbrime@gmail.com>
2022-03-22 00:11:30 +0900

    Project's Initial Commit

---
+++ www/components/ocao.js
@@ -0,0 +1,6 @@
+//maria alencar dois (2)
+window.onload = function(){
+    document.querySelector('#alerta1').addEventListener('click', function(){
+        navigator.notification.alert('Pedro Alencar está a invadir minha residência');
+    });
+}
